1a4eea7103dacc4dea8f44e17d61a1499e250876
Ali Mohamed Taima <es-ali.mohamed2019@alexu.edu.eg>
2022-08-23 23:12:19 +0200

    server code and header files

--- Payment_Application/Server/server.c
+++ Payment_Application/Server/server.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "..\card\card.h"           //---------------delete this -----
-#include "..\terminal\terminal.h"	//---------------delete this -----
+#include "..\card\card.h"           
+#include "..\terminal\terminal.h"	
 #include "server.h"
+// define thee number of acounts I make in the database
+#define database_num 10
 //the valid accounts database
 ST_accountsDB_t database[255] = {	2000.0, RUNNING, "8989374615436851",
 									3000.0, BLOCKED, "6317671354844040",
@@ -16,8 +18,105 @@ ST_accountsDB_t database[255] = {	2000.0, RUNNING, "8989374615436851",
 									10000.0, RUNNING, "6173128629492300",
 									11000.0, BLOCKED, "5807007076043875"
 };
-int main()
+// intialize global array of ST_transaction_t with zeros
+ST_transaction_t transaction_data[255] = { 0, 0, 0, 0 };
+
+//int main()
+//{
+//	ST_terminalData_t termData;
+//	ST_cardData_t cardData;
+//	ST_transaction_t transData;
+//	
+//	// Getting card pan 
+//    if(getCardPAN(&cardData) == WRONG_PAN)
+//        printf("error! PAN is 20 numeric characters string, 19 character max, and 16 character min");
+//	else
+//	{	// Getting transaction amount
+//		if (getTransactionAmount(&termData) == INVALID_AMOUNT)
+//			printf("error! Invalid amount");
+//		else
+//		{	
+//			//printf("The transaction amount from getTransactionAmount function = %f\n", termData.transAmount);
+//			
+//			// Assigning card and terminal data to the trasdata
+//			transData.cardHolderData = cardData;
+//			transData.terminalData = termData;
+//			if (recieveTransactionData(&transData) == FRAUD_CARD)
+//				printf("error! Fraud card");
+//			else
+//			{
+//				if (recieveTransactionData(&transData) == DECLINED_STOLEN_CARD)
+//					printf("error! Declined stolen card");
+//				else
+//				{
+//					if (recieveTransactionData(&transData) == DECLINED_INSUFFECIENT_FUND)
+//						printf("error! Declined insuffecient fund");
+//					else
+//					{
+//						if (recieveTransactionData(&transData) == APPROVED)
+//							printf("-----------------Successful process-----------------");
+//					}
+//				}
+//			}
+//		}
+//	}
+//
+//	return 0;
+//}
+EN_transState_t recieveTransactionData(ST_transaction_t* transData)
 {
-	printf("the balance for index 2 = %.1f", database[2].balance);
-	return 0;
+	ST_accountsDB_t accountRefrence;
+
+	if (isValidAccount(&transData->cardHolderData, &accountRefrence) == ACCOUNT_NOT_FOUND)
+		return FRAUD_CARD;
+	else
+	{
+		/*printf("The balance of this account = %f\n", accountRefrence.balance);
+		printf("The account number = %s\n", accountRefrence.primaryAccountNumber);*/
+		if (isBlockedAccount(&accountRefrence) == BLOCKED_ACCOUNT)
+			return DECLINED_STOLEN_CARD;
+		else
+		{
+			if (isAmountAvailable(&transData->terminalData, &accountRefrence) == LOW_BALANCE)
+				return DECLINED_INSUFFECIENT_FUND;
+			else
+			{	
+				// I will call the serverTransaction function here after implement it
+				return APPROVED;
+			}
+		}
+	}
+}
+EN_serverError_t isValidAccount(ST_cardData_t* cardData, ST_accountsDB_t* accountRefrence)
+{
+	int a;
+	for (a = 0; a < database_num; a++)
+	{
+		/* strcmp function return 0 if the two strings are identical so I put not before it to be true only 
+		 if the return value of strcmp is 0*/
+		if (!strcmp(database[a].primaryAccountNumber, cardData->primaryAccountNumber))
+		{
+			// assigning the data of account to account reference pointer
+			*accountRefrence = database[a];
+			return SERVER_OK;
+		}
+	}
+	return ACCOUNT_NOT_FOUND;
+}
+EN_serverError_t isBlockedAccount(ST_accountsDB_t* accountRefrence)
+{
+	 
+	if (accountRefrence->state == BLOCKED)
+		return BLOCKED_ACCOUNT;
+	else
+		return SERVER_OK;
+		
+}
+EN_serverError_t isAmountAvailable(ST_terminalData_t* termData, ST_accountsDB_t* accountRefrence)
+{
+	// in this function I add additional pointer of ST_accountsDB_t* accountRefrence to use its data in the comparison tha I make
+	if (termData->transAmount > accountRefrence->balance)
+		return LOW_BALANCE;
+	else
+		return SERVER_OK;
 }
